d54e0129728bda585bc64dad194cb194116ec0d0
kurosaki-ui <p10p030@cis.ac.jp>
2020-07-04 00:16:15 +0900

    ScriptTest初めてのコミット
    Lesson4 提出課題作成

---
+++ Assets/Test.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss 
+{
+	private int hp =100;	
+	private int power = 25;
+	private int mp = 53;
+	public void Attack()
+	{
+		Debug.Log(this.power + "のダメージを与えた");
+	}
+	public void Defence(int damage)
+	{
+		Debug.Log(damage + "ダメージを受けた");
+		this.hp -= damage;
+	}
+	public void MagicAttack()
+	{
+		if(this.mp > 5)
+		{
+			this.mp = mp - 5;
+			Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+		}else
+		{
+			Debug.Log("MPが足りないため魔法が使えない。");				
+		}
+	}
+}
+
+public class Test : MonoBehaviour 
+{
+	int[] array = {10, 20, 30, 40, 50};
+
+	int Add(int a, int b, int c)
+	{
+		int d = a + b + c;
+		return d;
+	}	
+	// Use this for initialization
+	void Start () 
+	{
+		int num = Add(3, 6, 5);
+		Debug.Log(num);
+
+		Boss midboss = new Boss();
+		midboss.Attack();
+		midboss.Defence(3);
+
+		for(int i=0; i < 11; i++)
+		{
+			midboss.MagicAttack();
+		}
+		
+
+		for(int i = 4; i >= 0; i--)
+		{
+			Debug.Log(array[i]);
+		}
+	}
+		
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
